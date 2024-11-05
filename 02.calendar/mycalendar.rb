ed8f349d581a6ba7260e72a2f80f0f0ea74ec6b0
e-yanagita-gs <e.yanagita@genbasupport.com>
2024-11-05 12:20:45 +0900

    calender fix

--- 02.calendar/mycalendar.rb
+++ 02.calendar/mycalendar.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
+require 'optparse'
+require 'date'
 
 #引数（m:月、y:年）を設定
-require 'optparse'
 opt = OptionParser.new
 
 params = {}
@@ -12,16 +13,6 @@ opt.on('-y YEAR',Integer)  {|v| v}
 opt.parse!(ARGV,into: params)
 
 #引数がない場合、今日の月・年を取得
-require 'date'
-if params.empty?
-  params = {}
-  today = Date.today
-  params[:m] = today.month
-  params[:y] = today.year
-end
-
-#引数がない場合、今日の月・年を取得
-require 'date'
 if params.empty?
   params = {}
   today = Date.today
@@ -30,33 +21,26 @@ if params.empty?
 end
 
 #見出し：年・月・曜日の表示
-d = Date.new(params[:y],params[:m],1).strftime('%B') #月の名称の取得
-puts "#{d} #{params[:y]}".center(20)
+first_date = Date.new(params[:y], params[:m], 1)
+puts "#{first_date.strftime('%B')} #{params[:y]}".center(20)
 day_of_week = ["Su","Mo","Tu","We","Th","Fr","Sa"]
-day_of_week.each do |x|
-  print x + " "
-end
-print "\n"
+puts day_of_week.join(' ')
 
 #指定月の１日の曜日を求めて該当する曜日の下に記載
 start_day = 1
-start_day_w = Date.new(params[:y],params[:m],1).wday #１日の曜日を取得
-print start_day.to_s.rjust(3*start_day_w+2) + " " 
-if start_day_w == 6
+start_day_w = first_date.wday #１日の曜日を取得
+print start_day.to_s.rjust(3 * start_day_w + 2) + " " 
+if first_date.saturday?
   print "\n"
 end
 
 #指定月の２日～最終日のカレンダーを作成
 start_day += 1
-last_day = Date.new(params[:y],params[:m],-1)
+last_day = Date.new(params[:y], params[:m], -1)
 while start_day <= last_day.day
-  if start_day < 10
-    print start_day.to_s.rjust(2) + " "
-  else
-    print start_day.to_s + " "
-  end
-  weekday = Date.new(params[:y],params[:m],start_day)
-  if weekday in wday:6
+  print start_day.to_s.rjust(2) + " "
+  weekday = Date.new(params[:y], params[:m], start_day)
+  if weekday.saturday?
     print "\n"
   end
   start_day += 1
