e9b2457c1a9e73231ae636a5c7f3b95b825af04f
e-yanagita-gs <e.yanagita@genbasupport.com>
2024-11-14 14:17:01 +0900

    revised

--- 03.bowling/mybowling.rb
+++ 03.bowling/mybowling.rb
@@ -3,43 +3,37 @@
 
 score = ARGV[0]
 scores = score.split(',')
-input_frames = []
-edit_frames = []
 
-# 入力されたスコアを整数値の配列(input_frames)に変換
-scores.flat_map do |s|
-  fallen_pin = s == 'X' ? 10 : s.to_i
-  input_frames.push(fallen_pin)
+# 入力されたスコアを整数値の配列(frames)に変換
+frames = scores.map do |s|
+  s == 'X' ? 10 : s.to_i
 end
 
-# スコアを２投（１フレーム）ずつにスライス(edit_framess)
+# スコアを２投（１フレーム）ずつにスライス(edit_frames)
 # ただし、ストライクの場合は１投１フレームになるようスライス
 count = 0
-while count < input_frames.size
-  if input_frames[count] == 10
-    edit_frames << [input_frames[count]]
-    count += 1
-  else
-    edit_frames << input_frames[count, 2]
-    count += 2
-  end
+edit_frames = []
+while count < frames.size
+  separator = frames[count] == 10 ? 1 : 2
+  edit_frames << frames[count, separator]
+  count += separator
 end
 
 # ストライク・スペア時のボーナスポイントの計算
 def calc_bonus_point(frame, frames, count)
-  if frame[0] == 10 # ストライク
-    frames[count + 1..].flatten.take(2).sum
-  elsif frame.sum == 10 # スペア
-    frames[count + 1..].flatten.take(1).sum
-  else
-    0
-  end
+  bonus_count = if frame[0] == 10      # ストライク
+                  2
+                elsif frame.sum == 10  # スペア
+                  1
+                else
+                  0
+                end
+  frames[count + 1..].flatten.take(bonus_count).sum
 end
 
 # 総スコアの計算
-total_point = 0
-edit_frames.take(10).each_with_index do |raw_score, index|
-  total_point += raw_score.sum + calc_bonus_point(raw_score, edit_frames, index)
+total_point = edit_frames.take(10).each_with_index.sum do |raw_score, index|
+  raw_score.sum + calc_bonus_point(raw_score, edit_frames, index)
 end
 
 puts total_point
