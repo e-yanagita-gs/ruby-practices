19409f8fd0e051e979b19482b6d1bcea7a93a0a3
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-18 15:08:59 +0900

    shotをフレームごとに分割するGameクラスのinitializeメソッドを追加

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -3,4 +3,19 @@
 require_relative 'frame'
 
 class Game
+  def initialize(mark)
+    marks = mark.split(',')
+    count = 0
+    @frames = []
+    while count < marks.size && @frames.size < 9
+      if marks[count] = 'X'
+        @frames << Frame.new('X', '0')
+        count += 1
+      else
+        @frames << Frame.new(marks[count], marks[count + 1])
+        count += 2
+      end
+    end
+    @frames << Frame.new(marks[count], marks[count + 1], marks[count + 2])
+  end
 end
