1b09299881186170533f37f9cd5d53c715086677
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-18 16:48:42 +0900

    スコア計算のためのメソッドを追加

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -8,7 +8,7 @@ class Game
     count = 0
     @frames = []
     while count < marks.size && @frames.size < 9
-      if marks[count] = 'X'
+      if marks[count] == 'X'
         @frames << Frame.new('X', '0')
         count += 1
       else
@@ -17,5 +17,22 @@ class Game
       end
     end
     @frames << Frame.new(marks[count], marks[count + 1], marks[count + 2])
+  end
+
+  def total_score
+    @frames.each_with_index.sum do |frame, index|
+      frame.score + bonus_score(frame, index)
+    end
+  end
+
+  def bonus_score(frame, index)
+    bonus_count = if frame.strike?
+                    2
+                  elsif frame.spare?
+                    1
+                  else
+                    0
+                  end
+    @frames[index + 1..].flat_map(&:shots).take(bonus_count).map(&:score).sum
   end
 end
