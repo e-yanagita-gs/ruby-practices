ad6a9eb213060e51c996665734cbe2ee0c52631d
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-26 13:54:00 +0900

    不要なコメントの削除

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -5,31 +5,17 @@ require_relative 'shot'
 class Frame
   def initialize(first_mark, second_mark = nil, third_mark = nil)
     @frame = [first_mark, second_mark, third_mark].compact.map { |mark| Shot.new(mark) }
-    #@first_shot = Shot.new(first_mark)
-    #@second_shot = Shot.new(second_mark)
-    #@third_shot = Shot.new(third_mark)
   end
 
-=begin
-  def shots
-    [@first_shot, @second_shot, @third_shot].compact
-  end
-=end
-
-  #def score(count_shot = @frame.size)
   def calc_score(count_shot = @frame.size)
-    #@first_shot.score + @second_shot.score + @third_shot.score
-    #@frame[0].score + @frame[1].score + @frame[2].score
     @frame.take(count_shot).sum(&:score)
   end
 
   def strike?
-    #@first_shot.score == 10
     @frame.first.score == 10
   end
 
   def spare?
-    #@frame.score == 10 && !strike?
     calc_score == 10 && !strike?
   end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -5,44 +5,12 @@ require_relative 'frame'
 
 class Game
   def initialize(input_marks)
-    #marks = input_marks.split(',')
-=begin
-    @frames = marks.map { |mark| Frame.new(mark) }
-    print @frames
-    puts
-    output_marks = @frames.map do |frame|
-      frame.instance_variable_get(:@frame).map(&:score)
-    end.flatten
-
-    print output_marks
-=end
-
-=begin
-  # Xを10に変換してからフレーム分け
-    marks = input_marks.split(',').map { |mark| mark == 'X' ? '10' : mark }
-    count = 0
-    @frames = []
-    while count < marks.size && @frames.size < 9
-      if marks[count] == '10'
-        @frames << Frame.new('10')
-        count += 1
-      else
-        @frames << Frame.new(marks[count], marks[count + 1])
-        count += 2
-      end
-    end
-    @frames << Frame.new(marks[count], marks[count + 1], marks[count + 2]
-=end
-
-  # Xそのままでフレーム分け
     marks = input_marks.split(',')
     count = 0
     @frames = []
     while count < marks.size && @frames.size < 9
-      #if marks[count] == 'X'
       if Shot.new(marks[count]).score == 10
         puts "X:#{Shot.new(marks[count]).score}"
-        #@frames << Frame.new('X')
         @frames << Frame.new(10)
         count += 1
       else
@@ -57,7 +25,6 @@ class Game
 
   def total_score
     @frames.each_with_index.sum do |frame, index|
-      #frame.score + bonus_score(frame, index)
       frame.calc_score + bonus_score(frame, index)
     end
   end
@@ -78,19 +45,13 @@ class Game
 
   def strike_bonus(index)
     if @frames[index + 1].strike? && @frames[index + 2] && index < 8
-      #@frames[index + 1].shots.first.score + @frames[index + 2].shots.first.score
-      #@frames[index + 1].score(1) + @frames[index + 2].score(1)
       @frames[index + 1].calc_score(1) + @frames[index + 2].calc_score(1)
     else
-      #@frames[index + 1].shots.take(2).map(&:score).sum
-      #@frames[index + 1].score(2)
       @frames[index + 1].calc_score(2)
     end
   end
 
   def spare_bonus(index)
-    #@frames[index + 1].shots.first.score
-    #@frames[index + 1].score(1)
     @frames[index + 1].calc_score(1)
   end
 end
