4304810672922e9fe8da713e1bc49371ebee73c6
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-04-08 13:27:59 +0900

    不要な変数の削除

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -7,15 +7,13 @@ class Game
   def initialize(input_text)
     shots = input_text.split(',').map { |mark| Shot.new(mark) }
     count = 0
-    frame_index = 0
     @frames = []
     while count < shots.size && @frames.size < 9
       shots_per_frame = shots[count].strike? ? 1 : 2
-      @frames << Frame.new(frame_index, *shots[count, shots_per_frame])
-      frame_index += 1
+      @frames << Frame.new(@frames.size, *shots[count, shots_per_frame])
       count += shots_per_frame
     end
-    @frames << Frame.new(frame_index, *shots[count..])
+    @frames << Frame.new(@frames.size, *shots[count..])
   end
 
   def calc_total_score
